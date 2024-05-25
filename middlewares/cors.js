d05adb2052755e4306316fc41d049cedd10a2edb
m1tka09 <149705488+m1tka09@users.noreply.github.com>
2024-05-25 20:21:03 +0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,9 +2,9 @@ const allowedCors = ["https://frontsaba.nomoredomainswork.ru"];
 
 function cors(req, res, next) {
   const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
+  // if (allowedCors.includes(origin)) {
+  //   res.header("Access-Control-Allow-Origin", origin);
+  // }
   res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
   res.header(
     "Access-Control-Allow-Headers",
