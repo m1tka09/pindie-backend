d7a0b368ee6ae06b4485dcdd12210028a0ae07ce
m1tka09 <149705488+m1tka09@users.noreply.github.com>
2024-05-25 23:33:04 +0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,4 +1,4 @@
-const allowedCors = ["https://frontsaba.nomoredomainswork.ru/"];
+const allowedCors = ["https://frontsaba.nomoredomainswork.ru/api"];
 
 function cors(req, res, next) {
   const { origin } = req.headers;
